4347c7d74654902336a1ebecb4ba6d261156de06
Zachary Baca <zachary.baca@msn.com>
2024-04-09 14:21:07 -0500

    restructuring form

---
+++ index.js
@@ -0,0 +1,10 @@
+// Grab Important Elements, and Assign Variables to Them
+const submitButton = document.getElementById('submit-button');
+const form = document.baddies;
+
+
+// Add Event Listener on Submit Button
+submitButton.addEventListener('submit', (e) => {
+    // Grab Type of Baddie
+    const typeOfBaddie = form.baddie.value;
+})
