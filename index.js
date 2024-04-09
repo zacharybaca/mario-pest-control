0287131d4ba0a76bc092b769d5f347be61f3998b
Zachary Baca <zachary.baca@msn.com>
2024-04-09 18:35:05 -0500

    made some form changes and injected images of baddies

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 // Grab Important Elements, and Assign Variables to Them
 const submitButton = document.getElementById('submit-button');
 const form = document.baddies;
+const priceField = document.getElementById('price');
+let price = document.getElementById('num');
+let typeOfBaddie = form.baddie;
+ // Create Image Element to Inject Image in Form
+let baddieImage = document.createElement('img');
 
+// Set Default Image to Goomba When Form Loads
+baddieImage.src = '/assets/goomba.png';
+priceField.appendChild(baddieImage);
+
+// Add Event Listener on Type Field For Changes
+// Then Update Price Field Accordingly
+// As Well As Injecting Image
+typeOfBaddie.addEventListener('change', (e) => {
+
+    if (e.target.value === 'goombas') {
+        price.innerText = '';
+        price.innerText = '5';
+        baddieImage.src = '/assets/goomba.png';
+    } else if (e.target.value === 'bob-ombs') {
+        price.innerText = '';
+        price.innerText = '7';
+        baddieImage.src = '/assets/bombs.jpeg';
+    } else {
+        price.innerText = '';
+        price.innerText = '11';
+        baddieImage.src = '/assets/cheep.jpeg';
+    }
+    priceField.appendChild(baddieImage);
+});
 
 // Add Event Listener on Submit Button
 submitButton.addEventListener('submit', (e) => {
-    // Grab Type of Baddie
-    const typeOfBaddie = form.baddie.value;
+    // Prevent Page Reload
+    e.preventDefault();
+
+    // Get Price of Baddie
+    // Multiply Price By Quantity Input Field Value
+    // Update Total Price of Baddies Caught
 })
